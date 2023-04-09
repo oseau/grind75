bd64a2946a3fb45256668f6373326a7390a5aa41
yang <oseau@me.com>
2023-04-09 20:28:11 +0800

    update

--- contentScript.js
+++ contentScript.js
@@ -1,13 +1,16 @@
+const $$ = document.querySelectorAll.bind(document)
+
 const action = async (start = 0) => {
-  const length = parseInt(/\/.*?(\d+)/.exec([...document.querySelectorAll("h3")].find(el => el.textContent.includes("Completed")).textContent)[1])
+  const length = parseInt(/\/.*?(\d+)/.exec([...$$("h3")]
+    .find(el => el.textContent.includes("Completed")).textContent)[1])
   let idx = start
-  const getAllLink = () => document.querySelectorAll("a.text-indigo-600")
+  const getAllLink = () => $$("a.text-indigo-600")
   if (getAllLink().length != length) {
-    [...document.querySelectorAll("button[aria-label*=' Week ']")].map((b) => b.click())
+    [...$$("button[aria-label*=' Week ']")].map((b) => b.click())
     await new Promise(r => setTimeout(r, 2000));
-    [...document.querySelectorAll("button[aria-label*=' Week ']")].map((b) => b.click())
+    [...$$("button[aria-label*=' Week ']")].map((b) => b.click())
     await new Promise(r => setTimeout(r, 2000));
-    [...document.querySelectorAll("button[aria-label^='Expand Week ']")].map((b) => b.click())
+    [...$$("button[aria-label^='Expand Week ']")].map((b) => b.click())
   }
   const select = () => {
     const selection = window.getSelection()
@@ -22,7 +25,7 @@ const action = async (start = 0) => {
     })
   }
   const open = () => {
-    window.open(getAllLink()[idx].href.replace("leetcode.com/", "leetcode.cn/"), "_blank").focus()
+    window.open(getAllLink()[idx].href.replace(".com/", ".cn/"), "_blank").focus()
     next()
   }
   const prev = () => {
